d9ead64db1583571108bd7b7f184d1ead8e8f7b0
muzzammil shaikh <muzzammil194@gmail.com>
2024-08-10 22:41:28 +0500

    fix: issue

--- src/app/modules/itineraries/itineraries.component.ts
+++ src/app/modules/itineraries/itineraries.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
-import { ModalService } from '../../services/core/modal/modal.service';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { ModalService } from "../../services/core/modal/modal.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './itineraries.component.html',
-  styleUrl: './itineraries.component.css'
+  selector: "app-home",
+  templateUrl: "./itineraries.component.html",
+  styleUrl: "./itineraries.component.css",
 })
 export class ItinerariesComponent {
-  constructor(public router:Router,public modalService:ModalService) {
-  }
+  constructor(
+    public router: Router,
+    public modalService: ModalService
+  ) {}
   backButtonClick() {
-    console.log('back button clicked');
+    console.log("back button clicked");
     window.history.back();
   }
 
   searchButtonClick() {
-    console.log('search button clicked');
-    this.router.navigate(['/search/search']).then(r => console.log(r));
+    console.log("search button clicked");
+    this.router.navigate(["/search/"]).then((r) => console.log(r));
   }
 }
--- src/app/modules/itineraries/pages/builder/components/create-itinerary/create-itinerary.component.ts
+++ src/app/modules/itineraries/pages/builder/components/create-itinerary/create-itinerary.component.ts
@@ -1,20 +1,23 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import {ModalService} from "../../../../../../services/core/modal/modal.service";
-import {CreateItineraryModalComponent} from "../create-itinerary-modal/create-itinerary-modal.component";
+import { Component, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
+import { ModalService } from "../../../../../../services/core/modal/modal.service";
 
 @Component({
-  selector: 'app-create-itinerary',
-  templateUrl: './create-itinerary.component.html',
-  styleUrl: './create-itinerary.component.css',
-  encapsulation: ViewEncapsulation.None
+  selector: "app-create-itinerary",
+  templateUrl: "./create-itinerary.component.html",
+  styleUrl: "./create-itinerary.component.css",
+  encapsulation: ViewEncapsulation.None,
 })
-export class CreateItineraryComponent {
-  cssClass = ['create-itinerary-modal'];
-  constructor(public modalService: ModalService) {
-
+export class CreateItineraryComponent implements OnInit {
+  @ViewChild("testModal", { static: false })
+  testModal: CreateItineraryComponent;
+  enableBack: boolean = false;
+  cssClass = ["create-itinerary-modal"];
+  constructor(public modalService: ModalService) {}
+  ngOnInit(): void {
+    this.modalService.bottomToggleModal = false;
   }
 
-  openModal(){
+  openModal() {
     this.modalService.bottomToggleModal = !this.modalService.bottomToggleModal;
     // this.modalService.openModal(CreateItineraryModalComponent,this.cssClass);
   }
