1eb118f0f5582a31231be412ed9753da88163b3e
Nauman Ahmad <nauman.ahmad@codeninjaconsulting.com>
2025-02-07 12:48:13 +0500

    made the code readable

--- src/app/shared/components/custom-story-card/custom-story-card.component.ts
+++ src/app/shared/components/custom-story-card/custom-story-card.component.ts
@@ -42,33 +42,10 @@ export class CustomStoryCardComponent {
   ) {}
 
   ngOnInit(): void {
-    if (!this.isDraft) {
-      this.apiService.get('/assets/data.json').subscribe((data) => {
-        this.cardData = data as ICardData;
-        this.processedCardData = this.processCardData(this.cardData);
-        this.cdr.detectChanges();
-      });
-    } else {
-      this.cardData = {
-        upVotes: '',
-        downVotes: 5,
-        title: '',
-        userName: '',
-        timeAgo: '',
-        location: '',
-        views: 0,
-        comments: 0,
-        shares: 0,
-        users: 0,
-        imageSrc: '',
-        userimageSrc: '',
-        isDraft: true,
-      };
-      this.processedCardData = this.processCardData(this.cardData);
-    }
+    this.loadCardData();
   }
 
-  private nullCheck(data: string | number | null | undefined): boolean {
+  private InvalidDataCheck(data: string | number | null | undefined): boolean {
     return (
       data?.toString().length == 0 ||
       data == null ||
@@ -82,22 +59,60 @@ export class CustomStoryCardComponent {
   private processCardData(data: ICardData): ICardData {
     return {
       ...data,
-      upVotes: this.nullCheck(data.upVotes) ? '00' : data.upVotes,
-      title: this.nullCheck(data.title) ? 'Untitled Itinerary' : data.title,
-      userName: this.nullCheck(data.userName) ? 'You' : data.userName,
-      timeAgo: this.nullCheck(data.timeAgo) ? '' : data.timeAgo,
-      users: this.nullCheck(data.users) ? 0 : data.users,
-      views: this.nullCheck(data.views) ? 0 : data.views,
-      comments: this.nullCheck(data.comments) ? 0 : data.comments,
-      shares: this.nullCheck(data.shares) ? 0 : data.shares,
-      imageSrc: this.nullCheck(data.imageSrc) ? ICONS.untitled : data.imageSrc,
-      userimageSrc: this.nullCheck(data.userimageSrc)
+      upVotes: this.InvalidDataCheck(data.upVotes) ? '00' : data.upVotes,
+      title: this.InvalidDataCheck(data.title)
+        ? 'Untitled Itinerary'
+        : data.title,
+      userName: this.InvalidDataCheck(data.userName) ? 'You' : data.userName,
+      timeAgo: this.InvalidDataCheck(data.timeAgo) ? '' : data.timeAgo,
+      users: this.InvalidDataCheck(data.users) ? 0 : data.users,
+      views: this.InvalidDataCheck(data.views) ? 0 : data.views,
+      comments: this.InvalidDataCheck(data.comments) ? 0 : data.comments,
+      shares: this.InvalidDataCheck(data.shares) ? 0 : data.shares,
+      imageSrc: this.InvalidDataCheck(data.imageSrc)
+        ? ICONS.untitled
+        : data.imageSrc,
+      userimageSrc: this.InvalidDataCheck(data.userimageSrc)
         ? ICONS.notSpecified
         : data.userimageSrc,
-      location: this.nullCheck(data.location) ? 'Not Specified' : data.location,
+      location: this.InvalidDataCheck(data.location)
+        ? 'Not Specified'
+        : data.location,
     };
   }
 
+  loadCardData() {
+    if (!this.isDraft) {
+      this.apiService.get('/assets/data.json').subscribe((data) => {
+        this.cardData = data as ICardData;
+        this.processedCardData = this.processCardData(this.cardData);
+        this.cdr.detectChanges();
+      });
+    } else {
+      this.cardData = this.createDummyCard();
+      this.processedCardData = this.processCardData(this.cardData);
+    }
+  }
+  createDummyCard(): ICardData {
+    let card: ICardData;
+    card = {
+      upVotes: '',
+      downVotes: 5,
+      title: '',
+      userName: '',
+      timeAgo: '',
+      location: '',
+      views: 0,
+      comments: 0,
+      shares: 0,
+      users: 0,
+      imageSrc: '',
+      userimageSrc: '',
+      isDraft: true,
+    };
+
+    return card;
+  }
   handleMenueItemSelect(): void {}
   handleMenuItemChange(): void {}
   handleMenuClick(): void {}
