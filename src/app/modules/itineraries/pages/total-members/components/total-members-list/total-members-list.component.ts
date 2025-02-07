425ba46bcdbe464d8845d3846b79a537f81c731d
Nauman Ahmad <nauman.ahmad@codeninjaconsulting.com>
2025-02-07 11:49:40 +0500

    location service added

--- src/app/modules/itineraries/pages/total-members/components/total-members-list/total-members-list.component.ts
+++ src/app/modules/itineraries/pages/total-members/components/total-members-list/total-members-list.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ICONS } from 'src/app/constants/constants';
@@ -13,9 +14,12 @@ export class TotalMembersListComponent {
   routeToSendInvites() {
     this.router.navigate([ItenariesRoutesEnum.SENDINVITES]);
   }
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private _location: Location,
+  ) {}
   ICONS: Iicon = ICONS;
   backButton(): void {
-    window.history.back();
+    this._location.back();
   }
 }
