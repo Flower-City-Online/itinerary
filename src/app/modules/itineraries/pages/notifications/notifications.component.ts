a35d656fb95bd25c75f63255303226e7f8bf3f20
Nauman Ahmad <nauman.ahmad@codeninjaconsulting.com>
2025-02-07 11:45:26 +0500

    location service added

--- src/app/modules/itineraries/pages/notifications/notifications.component.ts
+++ src/app/modules/itineraries/pages/notifications/notifications.component.ts
@@ -1,12 +1,13 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.scss',
 })
 export class NotificationsComponent {
+  constructor(private _location: Location) {}
   backButton(): void {
-    window.history.back();
+    this._location.back();
   }
 }
